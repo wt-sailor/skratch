9fa11743b7bde05ffb3b82e4ef9326b8f94eaf84
Umang Sailor <umang@weetechsolution.com>
2026-05-07 12:22:48 +0530

    feat: initialize NestJS application with user and report modules
    - Added package.json with dependencies and scripts for NestJS application.
    - Created AppController and AppService for basic functionality.
    - Implemented UsersModule and ReportsModule with respective controllers and services.
    - Defined User and Report entities with TypeORM.
    - Added DTOs for user creation and updates.
    - Implemented serialization interceptor for response formatting.
    - Set up global validation pipe in main application entry point.
    - Added unit and e2e tests for controllers and services.

---
+++ mycv/src/users/users.controller.ts
@@ -0,0 +1,53 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { Serialize } from 'src/interceptors/sterilize.interceptor';
+import { UserDto } from './dtos/user.dto';
+
+@Controller('auth')
+@Serialize(UserDto)
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    return this.usersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    console.log('Handler is running');
+
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
+  }
+}
---
+++ mycv/src/users/users.entity.ts
@@ -0,0 +1,18 @@
+import { AfterInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted user with id: ', this.id);
+  }
+}
